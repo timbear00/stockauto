5500de64103fe3a8fe4c93c512561805c4d3be4d
timbear00 <34996716+timbear00@users.noreply.github.com>
2021-09-27 18:56:46 +0900

    Update AutoTrade.py
    token 인터넷 비공개를 위한 commit

--- AutoTrade.py
+++ AutoTrade.py
@@ -17,7 +17,7 @@ def post_message(token, channel, text):
         data={"channel": channel,"text": text}
     )
 
-myToken = "xoxb-2148803419396-2166465567280-TWmTgUrWJ7T9gdcd7L6tbF2R"
+myToken = "" # your Token
 def dbgout(message):
     """인자로 받은 문자열을 파이썬 셸과 슬랙으로 동시에 출력한다."""
     print(datetime.now().strftime('[%m/%d %H:%M:%S]'), message)
